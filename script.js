d4134e4b95edde8c223092b1b61a0535b970ced7
BrightCat14 <107373258+BrightCat14@users.noreply.github.com>
2024-09-25 11:20:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggleBtn = document.getElementById('theme-toggle');
     const body = document.body;
 
-    themeToggleBtn.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-        themeToggleBtn.textContent = body.classList.contains('dark-mode') ? 'Светлая тема' : 'Темная тема';
-
-        if (body.classList.contains('dark-mode')) {
-            localStorage.setItem('theme', 'dark');
-        } else {
-            localStorage.setItem('theme', 'light');
-        }
-    });
-
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
         body.classList.add('dark-mode');
-        themeToggleBtn.textContent = 'Светлая тема';
     }
 });
